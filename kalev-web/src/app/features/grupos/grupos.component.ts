1bdbbb1d92ce636a3f96b220fff5393a7dd01f49
Martin <olivarezmartin714@gmail.com>
2025-02-07 03:33:53 -0600

    First commit -tinn505

---
+++ kalev-web/src/app/features/grupos/grupos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grupos',
+  standalone: false,
+  templateUrl: './grupos.component.html',
+  styleUrl: './grupos.component.scss'
+})
+export class GruposComponent {
+
+}
