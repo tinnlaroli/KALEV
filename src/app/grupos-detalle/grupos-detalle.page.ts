872e09fa75d328139bd7331bdbbbf0242bd63532
Ferchiisp <20233l001037@utcv.edu.mx>
2025-02-12 10:36:10 -0600

    feat: agregar modal que mostrara detalle de actividades

--- src/app/grupos-detalle/grupos-detalle.page.ts
+++ src/app/grupos-detalle/grupos-detalle.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-grupos-detalle',
   templateUrl: './grupos-detalle.page.html',
   styleUrls: ['./grupos-detalle.page.scss'],
+  standalone: false
 })
-export class GruposDetallePage implements OnInit {
+export class GruposDetallePage {
+  @Input() grupo: any; // Recibimos el grupo seleccionado
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) {}
 
-  ngOnInit() {
+  cerrar() {
+    this.modalCtrl.dismiss();
   }
-
 }
