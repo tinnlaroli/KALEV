3c2779c9c4966dae2fe0432ab62788583bf09c9e
Ferchiisp <20233l001037@utcv.edu.mx>
2025-02-22 17:31:05 -0600

    docs: agregar documentación a funciones en calificaciones.module.ts

--- src/app/calificaciones/calificaciones.module.ts
+++ src/app/calificaciones/calificaciones.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';  // Importación de NgModule para la configuración del módulo
+import { CommonModule } from '@angular/common';  // Importación de módulos comunes necesarios
+import { FormsModule } from '@angular/forms';  // Importación de FormsModule para trabajar con formularios en Angular
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';  // Importación del módulo Ionic para funcionalidades de Ionic
 
-import { CalificacionesPageRoutingModule } from './calificaciones-routing.module';
+import { CalificacionesPageRoutingModule } from './calificaciones-routing.module';  // Importación del módulo de rutas específico para la página de calificaciones
 
-import { CalificacionesPage } from './calificaciones.page';
+import { CalificacionesPage } from './calificaciones.page';  // Importación del componente de la página de calificaciones
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CalificacionesPageRoutingModule
+    // Se importan los módulos necesarios para este módulo
+    CommonModule,  // Módulo común de Angular
+    FormsModule,  // Módulo de formularios de Angular
+    IonicModule,  // Módulo principal de Ionic
+    CalificacionesPageRoutingModule  // Módulo de rutas de la página de calificaciones
   ],
-  declarations: [CalificacionesPage]
+  declarations: [CalificacionesPage]  // Declaración del componente de la página de calificaciones
 })
-export class CalificacionesPageModule {}
+export class CalificacionesPageModule {}  // Declaración del módulo para la página de calificaciones
