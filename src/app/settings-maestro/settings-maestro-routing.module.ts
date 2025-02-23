4e7e0ac438b492e8a9476da343556f06cf5ed393
Ferchiisp <20233l001037@utcv.edu.mx>
2025-02-22 18:59:42 -0600

    docs: agregar documentación a funciones en settings-maestro-routing.module.ts

--- src/app/settings-maestro/settings-maestro-routing.module.ts
+++ src/app/settings-maestro/settings-maestro-routing.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+// Importación de módulos necesarios para configurar el enrutamiento en Angular
+import { NgModule } from '@angular/core'; // Importa el decorador NgModule para definir un módulo de Angular
+import { Routes, RouterModule } from '@angular/router'; // Importa los módulos Routes y RouterModule para configurar el enrutamiento en Angular
 
-import { SettingsMaestroPage } from './settings-maestro.page';
+import { SettingsMaestroPage } from './settings-maestro.page'; // Importa el componente de la página de configuración del maestro
 
+// Definición de las rutas para la página de configuración del maestro
 const routes: Routes = [
   {
-    path: '',
-    component: SettingsMaestroPage
+    path: '', // Ruta raíz (cuando se accede a esta ruta sin especificar ningún subcamino)
+    component: SettingsMaestroPage // El componente que se cargará cuando se acceda a la ruta
   }
 ];
 
+// Definición del módulo SettingsMaestroPageRoutingModule
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)], // Configura el enrutamiento utilizando RouterModule.forChild para rutas hijo (no principales)
+  exports: [RouterModule], // Exporta RouterModule para que otras partes de la aplicación puedan acceder a las rutas definidas
 })
-export class SettingsMaestroPageRoutingModule {}
+export class SettingsMaestroPageRoutingModule {} // Exporta el módulo para su uso en otros módulos
