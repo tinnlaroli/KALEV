b477729702a349292de8a64e6f18f2d07ae34a42
Ferchiisp <20233l001037@utcv.edu.mx>
2025-02-22 18:36:33 -0600

    docs: agregar documentación a funciones en profile.module.ts

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+// Importación de los módulos necesarios para el módulo de ProfilePage
+import { NgModule } from '@angular/core';  // Importación del decorador 'NgModule' de Angular
+import { CommonModule } from '@angular/common';  // Importación del módulo común de Angular que proporciona funcionalidades esenciales
+import { FormsModule } from '@angular/forms';  // Importación del módulo de formularios para usar formularios en la aplicación
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';  // Importación del módulo de Ionic para integrar las funcionalidades de Ionic
 
+// Importación del módulo de enrutamiento específico para la página de perfil
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
+// Importación del componente 'ProfilePage' que representa la página de perfil
 import { ProfilePage } from './profile.page';
 
+// Definición del módulo de la página de perfil
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ProfilePageRoutingModule
+  imports: [  // Lista de módulos que este módulo necesita
+    CommonModule,  // Módulo común de Angular
+    FormsModule,  // Módulo de formularios de Angular
+    IonicModule,  // Módulo de Ionic
+    ProfilePageRoutingModule  // Módulo de enrutamiento para la página de perfil
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage]  // Declaración del componente 'ProfilePage' para que Angular lo reconozca en este módulo
 })
+// Exportación del módulo ProfilePageModule, que encapsula todo lo necesario para la página de perfil
 export class ProfilePageModule {}
