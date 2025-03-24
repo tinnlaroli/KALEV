c0e7ee04d7c3126afb978b839988d9fd5170aa5e
Ferchiisp <20233l001037@utcv.edu.mx>
2025-03-24 17:47:58 -0600

    feat: agregar lógica para cargar perfil del estudiante

--- src/app/features/students/student-profile/student-profile.component.ts
+++ src/app/features/students/student-profile/student-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StudentService } from '../../../services/student.service';
 
 @Component({
   selector: 'app-student-profile',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './student-profile.component.html',
   styleUrl: './student-profile.component.css'
 })
-export class StudentProfileComponent {
+export class StudentProfileComponent implements OnInit {
+  student: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private studentService: StudentService
+  ) {}
+
+  ngOnInit(): void {
+    const studentId = this.route.snapshot.paramMap.get('id');
+    if (studentId) {
+      this.studentService.getById(studentId).subscribe({
+        next: (data) => this.student = data,
+        error: (err) => console.error('Error al cargar el perfil del estudiante', err)
+      });
+    }
+  }
 }
