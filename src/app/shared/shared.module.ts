f45d6e4f809c37fa8e748dc797a28a6d2df5b6c4
tinn505 <martinlaraolivares@example.com>
2025-03-27 10:16:28 -0600

    feat: alert global

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideNavComponent } from './side-nav/side-nav.component';  // Asegúrate de importar el componente
+import { ToastComponent } from './toast/toast.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     SideNavComponent,  // Declara los componentes que compartes
+    ToastComponent
   ],
   imports: [
     CommonModule,  // Importa módulos comunes,
     RouterModule
   ],
   exports: [
-    SideNavComponent  // Exporta los componentes para que otros módulos los usen
+    SideNavComponent,  // Exporta los componentes para que otros módulos los usen
+    ToastComponent
   ]
 })
 export class SharedModule { }
---
+++ src/app/shared/toast/toast.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-toast',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.css'],
+})
+export class ToastComponent {
+  @Input() message = '';
+  @Input() type: 'success' | 'error' | 'warning' = 'success';
+}
