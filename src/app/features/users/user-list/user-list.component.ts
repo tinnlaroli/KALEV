cdef1e75a3295401e00813485ff1d99ef5f50ff3
tinn505 <martinlaraolivares@example.com>
2025-03-27 08:14:56 -0600

    feat: superAdmin gestion usuarios

--- src/app/features/users/user-list/user-list.component.ts
+++ src/app/features/users/user-list/user-list.component.ts
@@ -10,7 +10,15 @@ import { UserService } from '../../../services/user.service';
 export class UserListComponent implements OnInit {
   users: any[] = [];
   selectedUser: any = { id: null, name: '', email: '', password: '' };
-  newUser = { name: '', email: '', password: '' };
+  newUser = {
+    nombre_usuario: '',
+    ap_paterno: '',
+    ap_materno: '',
+    correo: '',
+    telefono: '',
+    contrasenia: '',
+    id_rol: 3, // Rol por defecto de tutor(permisos)
+  };
 
   isCreateModalOpen = false;
   isEditModalOpen = false;
@@ -28,7 +36,7 @@ export class UserListComponent implements OnInit {
 
     if (this.rol_id === 4) {
       // Superadmin: ver todos los usuarios de todos los roles
-      const roles = [1, 2, 3 ,4]; // Si quieres incluir otros superadmins, agrega 4
+      const roles = [1, 2, 3, 4]; // Si quieres incluir otros superadmins, agrega 4
       this.users = []; // Asegúrate de limpiar antes
 
       roles.forEach((roleId) => {
@@ -56,15 +64,32 @@ export class UserListComponent implements OnInit {
   }
 
   createUser() {
-    if (!this.newUser.name || !this.newUser.email || !this.newUser.password)
+    if (
+      !this.newUser.nombre_usuario ||
+      !this.newUser.correo ||
+      !this.newUser.contrasenia ||
+      !this.newUser.id_rol ||
+      !this.newUser.ap_paterno ||
+      !this.newUser.ap_materno ||
+      !this.newUser.telefono
+    ) {
+      alert('Faltan datos');
       return;
-
+    }
     this.userService.createUser(this.newUser).subscribe({
       next: () => {
         alert('Usuario creado');
         this.getUsers();
         this.closeCreateModal();
-        this.newUser = { name: '', email: '', password: '' };
+        this.newUser = {
+          nombre_usuario: '',
+          ap_paterno: '',
+          ap_materno: '',
+          correo: '',
+          telefono: '',
+          contrasenia: '',
+          id_rol: 1,
+        };
       },
       error: (err) => console.error('Error al crear usuario:', err),
     });
@@ -73,10 +98,16 @@ export class UserListComponent implements OnInit {
   editUser(id: number) {
     this.userService.getUserById(id).subscribe({
       next: (data) => {
+        console.log('Datos del usuario:', data.data);
         this.selectedUser = {
-          id: data.id_usuario,
-          name: data.nombre_usuario,
-          email: data.correo,
+          id_usuario: data.data.id_usuario,
+          nombre_usuario: data.data.nombre_usuario,
+          ap_paterno: data.data.ap_paterno,
+          ap_materno: data.data.ap_materno,
+          correo: data.data.correo,
+          telefono: data.data.telefono,
+          contrasenia: data.data.contrasenia,
+          id_rol: 1,
         };
         this.isEditModalOpen = true;
       },
@@ -86,16 +117,43 @@ export class UserListComponent implements OnInit {
   }
 
   updateUser() {
-    this.userService
-      .updateUser(this.selectedUser.id, this.selectedUser)
-      .subscribe({
-        next: () => {
-          alert('Usuario actualizado');
-          this.getUsers();
-          this.closeEditModal();
-        },
-        error: (err) => console.error('Error al actualizar usuario:', err),
-      });
+    const id = this.selectedUser.id_usuario;
+
+    if (!id) {
+      alert('ID de usuario no válido');
+      return;
+    }
+
+    if (
+      !this.selectedUser.nombre_usuario ||
+      !this.selectedUser.ap_paterno ||
+      !this.selectedUser.ap_materno ||
+      !this.selectedUser.correo ||
+      !this.selectedUser.telefono ||
+      !this.selectedUser.contrasenia
+    ) {
+      alert('Faltan datos para actualizar');
+      return;
+    }
+
+    const updateData = {
+      nombre_usuario: this.selectedUser.nombre_usuario,
+      ap_paterno: this.selectedUser.ap_paterno,
+      ap_materno: this.selectedUser.ap_materno,
+      correo: this.selectedUser.correo,
+      telefono: this.selectedUser.telefono,
+      contrasenia: this.selectedUser.contrasenia,
+      id_rol: this.selectedUser.id_rol,
+    };
+
+    this.userService.updateUser(id, updateData).subscribe({
+      next: () => {
+        alert('Usuario actualizado');
+        this.getUsers();
+        this.closeEditModal();
+      },
+      error: (err) => console.error('Error al actualizar usuario:', err),
+    });
   }
 
   deleteUser(id: number) {
@@ -115,6 +173,15 @@ export class UserListComponent implements OnInit {
   }
 
   closeEditModal() {
+    this.selectedUser = {
+      id_usuario: null,
+      nombre_usuario: '',
+      ap_paterno: '',
+      ap_materno: '',
+      correo: '',
+      telefono: '',
+      contrasenia: '',
+    };
     this.isEditModalOpen = false;
   }
 }
